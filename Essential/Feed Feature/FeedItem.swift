434e7a4dead2a8851b41d8afd822161aac941c98
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-04-14 23:02:53 +0530

    Create and configure new Essential developer project

---
+++ Essential/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Essential
+//
+//  Created by lakshman-7016 on 14/04/21.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
