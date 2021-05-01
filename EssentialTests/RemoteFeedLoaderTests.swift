259cb2f9188f8594399a3dcf89f9bcad5b4f757a
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-05-01 17:44:54 +0530

    Add success, failure states into result type

--- EssentialTests/RemoteFeedLoaderTests.swift
+++ EssentialTests/RemoteFeedLoaderTests.swift
@@ -71,24 +71,24 @@ class RemoteFeedLoaderTests: XCTestCase {
 	}
 
 	private class HTTPClientSpy: HTTPClient {
-		var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+		var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
 
 		var requestedURLs: [URL] {
 			return messages.map { $0.url }
 		}
 
-		func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+		func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
 			messages.append((url, completion))
 		}
 
 		func complete(with error: Error, at index: Int = 0) {
-			messages[index].completion(error, nil)
+			messages[index].completion(.failure(error))
 		}
 
 		func complete(with code: Int, at index: Int = 0) {
 			let url = messages[index].url
-			let response = HTTPURLResponse(url: url, statusCode: code, httpVersion: nil, headerFields: nil)
-			messages[index].completion(nil, response)
+			let response = HTTPURLResponse(url: url, statusCode: code, httpVersion: nil, headerFields: nil)!
+			messages[index].completion(.success(response))
 		}
 	}
 }
