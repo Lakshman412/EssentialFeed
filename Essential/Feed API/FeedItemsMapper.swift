a9cb0a48edd0a4fd88cfa46ecc2ccbba3f9640d0
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-05-04 23:05:05 +0530

    Move all mapping logic to FeedItemsMapper

--- Essential/Feed API/FeedItemsMapper.swift
+++ Essential/Feed API/FeedItemsMapper.swift
@@ -10,6 +10,10 @@ import Foundation
 internal final class FeedItemsMapper {
 	private struct Root: Decodable {
 		let items: [Item]
+
+		var feed: [FeedItem] {
+			return items.map { $0.item }
+		}
 	}
 
 	private struct Item: Decodable {
@@ -32,5 +36,13 @@ internal final class FeedItemsMapper {
 
 		let root = try JSONDecoder().decode(Root.self, from: data)
 		return root.items.map { $0.item }
+	}
+
+	static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+		guard response.statusCode == 200,
+			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			return .failure(.invalidData)
+		}
+		return .success(root.feed)
 	}
 }
