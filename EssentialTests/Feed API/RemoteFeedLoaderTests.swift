cf188cfebbb05268c5e49e70587e5b5b53722ce4
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-05-14 17:48:23 +0530

    Remove generic error type form LoadFeedResult

--- EssentialTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialTests/Feed API/RemoteFeedLoaderTests.swift
@@ -38,7 +38,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 	func test_load_deliversErrorOnClientErrors() {
 		let (sut, client) = makeSUT()
 
-		expect(sut, toCompleteWith: .failure(.connectivity), when: {
+		expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
 			let clientError = NSError(domain: "Test", code: 0)
 			client.complete(with: clientError)
 		})
@@ -50,7 +50,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 		let samples = [199, 201, 300, 400, 500]
 
 		samples.enumerated().forEach { index, code in
-			expect(sut, toCompleteWith: .failure(.invalidData), when: {
+			expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
 				let json = makeItemsJSON([])
 				client.complete(with: code, data: json, at: index)
 			})
@@ -60,7 +60,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 	func test_load_deliversError200HTTPResponseWithInvalidJSON() {
 		let (sut, client) = makeSUT()
 
-		expect(sut, toCompleteWith: .failure(.invalidData), when: {
+		expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
 			let data = Data("invalid json".utf8)
 			client.complete(with: 200, data: data)
 		})
@@ -155,7 +155,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 			switch (receivedResult, expectedResult) {
 			case let (.success(receivedItems), .success(expectedItems)):
 				XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-			case let (.failure(receivedError), .failure(expectedError)):
+			case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error)):
 				XCTAssertEqual(receivedError, expectedError, file: file, line: line)
 			default:
 				XCTFail("Expected Result \(expectedResult) got \(receivedResult)", file: file,line: line)
@@ -163,7 +163,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
 			exp.fulfill()
 		}
-		
+
 		action()
 
 		wait(for: [exp], timeout: 1.0)
