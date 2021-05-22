8f50b00fac5ce4f6f795211c0ce439244d0ee20e
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-05-22 19:56:04 +0530

    Move URL generation to a shared factory method

--- EssentialTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialTests/Feed API/URLSessionHTTPClientTests.swift
@@ -39,7 +39,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 	}
 
 	func test_getFromURL_performsGETRequestWithURL() {
-		let url = URL(string: "https://any-url.com")!
+		let url = anyURL()
 
 		let exp = expectation(description: "Wait for completion")
 		URLProtocolStub.observeRequests { request in
@@ -53,12 +53,11 @@ class URLSessionHTTPClientTests: XCTestCase {
 	}
 
 	func test_getFromURL_failsOnRequestError() {
-		let url = URL(string: "https://any-url.com")!
 		let error = NSError(domain: "any error", code: 1)
 		URLProtocolStub.stub(data: nil, response: nil, error: error)
 
 		let exp = expectation(description: "Wait for completion")
-		makeSUT().get(from: url) { result in
+		makeSUT().get(from: anyURL()) { result in
 			switch result {
 			case let .failure(requestError as NSError):
 				XCTAssertEqual(error, requestError)
@@ -76,6 +75,10 @@ class URLSessionHTTPClientTests: XCTestCase {
 		let sut = URLSessionHTTPClient()
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
+	}
+
+	private func anyURL() -> URL {
+		return URL(string: "https://any-url.com")!
 	}
 
 	private class URLProtocolStub: URLProtocol {
