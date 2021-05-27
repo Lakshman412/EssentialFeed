6fe572a6a0a853d838a5256b19d1ed1f7414eedb
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-05-27 22:15:17 +0530

    enable code coverage

--- Essential/Feed API/FeedItemsMapper.swift
+++ Essential/Feed API/FeedItemsMapper.swift
@@ -30,7 +30,7 @@ internal final class FeedItemsMapper {
 	static var OK_200: Int { return 200 }
 
 	static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-		guard response.statusCode == 200,
+		guard response.statusCode == OK_200,
 			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
