e18c5aa82816ade4090457f5d60531a18946b067
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-05-29 09:47:00 +0530

    extract test setup to a helper method

--- EssentialAPIEndToEndTests/EssentialAPIEndToEndTests.swift
+++ EssentialAPIEndToEndTests/EssentialAPIEndToEndTests.swift
@@ -10,21 +10,8 @@ import Essential
 
 class EssentialAPIEndToEndTests: XCTestCase {
 
-	func test_essentialFeedAPIEndToEnd() {
-		let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-		let client = URLSessionHTTPClient()
-		let loader = RemoteFeedLoader(url: testServerURL, client: client)
-
-		let exp = expectation(description: "Wait for completion")
-
-		var receivedResult: LoadFeedResult?
-		loader.load() { result in
-			receivedResult = result
-			exp.fulfill()
-		}
-
-		wait(for: [exp], timeout: 10.0)
-
+	func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+		let receivedResult = self.getFeedResult()
 		switch receivedResult {
 		case let .success(items):
 			XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
@@ -48,6 +35,23 @@ class EssentialAPIEndToEndTests: XCTestCase {
 	}
 
 	// MARK: - Helpers
+
+	private func getFeedResult() -> LoadFeedResult? {
+		let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+		let client = URLSessionHTTPClient()
+		let loader = RemoteFeedLoader(url: testServerURL, client: client)
+
+		let exp = expectation(description: "Wait for completion")
+
+		var receivedResult: LoadFeedResult?
+		loader.load() { result in
+			receivedResult = result
+			exp.fulfill()
+		}
+
+		wait(for: [exp], timeout: 10.0)
+		return receivedResult
+	}
 
 	private func expectedItem(at index: Int) -> FeedItem {
 		return FeedItem(
