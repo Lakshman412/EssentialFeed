cfc90baa80c7ddb49b23851c1dba661a434ab971
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-06-20 14:23:50 +0530

    add test cases to save command on deletion error and successful case

--- EssentialTests/Feed cache/CacheFeedUseCaseTests.swift
+++ EssentialTests/Feed cache/CacheFeedUseCaseTests.swift
@@ -16,15 +16,35 @@ class LocalFeedLoader {
 	}
 
 	func save(_ items: [FeedItem]) {
-		store.deleteCachedFeed()
+		store.deleteCachedFeed { [unowned self] error in
+			if error == nil {
+				self.store.insertFeed()
+			}
+		}
 	}
 }
 
 class FeedStore {
+	typealias DeletionCompletion = (Error?) -> Void
 	var deleteCAchedFeedCallCount = 0
+	var insertCallCount = 0
+	var deleteCallBack = [DeletionCompletion]()
 
-	func deleteCachedFeed() {
+	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		self.deleteCAchedFeedCallCount += 1
+		deleteCallBack.append(completion)
+	}
+
+	func insertFeed() {
+		self.insertCallCount += 1
+	}
+
+	func completeDeletion(with error: NSError, at index: Int = 0) {
+		deleteCallBack[index](error)
+	}
+
+	func completeDeletionSuccessfully(at index: Int = 0) {
+		deleteCallBack[index](nil)
 	}
 }
 
@@ -43,6 +63,27 @@ class CacheFeedUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.deleteCAchedFeedCallCount, 1)
 	}
 
+	func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+		let (sut, store) = makeSUT()
+		let items = [uniqueItems()]
+		let deleteCacheError = anyNSError()
+
+		sut.save(items)
+		store.completeDeletion(with: deleteCacheError)
+
+		XCTAssertEqual(store.deleteCAchedFeedCallCount, 1)
+	}
+
+	func test_save_requestsNewCacheInsertionOnSuccessfulDeletion() {
+		let (sut, store) = makeSUT()
+		let items = [uniqueItems()]
+
+		sut.save(items)
+		store.completeDeletionSuccessfully()
+		
+		XCTAssertEqual(store.insertCallCount, 1)
+	}
+
 	// MARK: - Helpers
 
 	func makeSUT(file: StaticString = #file, line: UInt = #line) -> (LocalFeedLoader, FeedStore) {
@@ -59,6 +100,10 @@ class CacheFeedUseCaseTests: XCTestCase {
 
 	private func anyURL() -> URL {
 		return URL(string: "https://any-url.com")!
+	}
+
+	private func anyNSError() -> NSError {
+		return NSError(domain: "any error", code: 0)
 	}
 }
 
