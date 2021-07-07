bb48985414e63f562f30c19d48e4bff3d47824cf
lakshman.surendhar <lakshman.surendhar@zohocorp.com>
2021-07-07 22:58:59 +0530

    add sut factory method

--- EssentialTests/Feed cache/CodableFeedStoreTests.swift
+++ EssentialTests/Feed cache/CodableFeedStoreTests.swift
@@ -73,7 +73,7 @@ class CodableFeedStoreTests: XCTestCase {
 	}
 
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 
 		let exp = expectation(description: "Wait for cache retrieval")
 
@@ -91,7 +91,7 @@ class CodableFeedStoreTests: XCTestCase {
 	}
 
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 
 		let exp = expectation(description: "Wait for cache retrieval")
 
@@ -111,7 +111,7 @@ class CodableFeedStoreTests: XCTestCase {
 	}
 
 	func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 		let feed = uniqueImageFeed().locals
 		let timestamp = Date()
 		let exp = expectation(description: "Wait for cache retrieval")
@@ -132,5 +132,13 @@ class CodableFeedStoreTests: XCTestCase {
 		}
 
 		wait(for: [exp], timeout: 1.0)
+	}
+
+	// MARK: - Helpers
+
+	func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
+		let sut = CodableFeedStore()
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return sut
 	}
 }
