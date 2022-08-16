7e60d6ffa743be3d5eb4690b88fde29df4beaab8
lachuman <sci.lakshman@gmail.com>
2022-08-16 23:26:37 +0530

    Add FeedPresenter holding feed loading presentation logic (loading state and loaded feed)

--- EssentialiOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialiOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,10 +12,11 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let feedViewModel = FeedViewModel(feedLoader: feedLoader)
-        let refeshController = FeedRefreshViewController(viewModel: feedViewModel)
+        let presenter = FeedPresenter(feedLoader: feedLoader)
+        let refeshController = FeedRefreshViewController(presenter: presenter)
         let feedController = FeedViewController(refreshController: refeshController)
-        feedViewModel.onFeedLoad = adaptFeedToCellController(forwardTo: feedController, imageLoader: imageLoader)
+        presenter.loadingView = refeshController
+        presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
         return feedController
     }
     
@@ -24,6 +25,22 @@ public final class FeedUIComposer {
             controller?.tableModel = feed.map { model in
                 FeedImageCellController(viewModel: FeedImageViewModel<UIImage>(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
             }
+        }
+    }
+}
+
+private final class FeedViewAdapter: FeedView {
+    private weak var controller: FeedViewController?
+    private var imageLoader: FeedImageDataLoader
+    
+    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(feed: [FeedImage]) {
+        controller?.tableModel = feed.map { model in
+            FeedImageCellController(viewModel: FeedImageViewModel<UIImage>(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
         }
     }
 }
---
+++ EssentialiOS/Feed UI/Models/FeedPresenter.swift
@@ -0,0 +1,40 @@
+//
+//  FeedPresenter.swift
+//  EssentialiOS
+//
+//  Created by lakshman-7016 on 16/08/22.
+//
+
+import Foundation
+import Essential
+
+protocol FeedLoadingView: AnyObject {
+    func display(isLoading: Bool)
+}
+
+protocol FeedView {
+    func display(feed: [FeedImage])
+}
+
+final class FeedPresenter {
+    typealias Observer<T> = (T) -> Void
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var feedView: FeedView?
+    weak var loadingView: FeedLoadingView?
+    
+    func loadFeed() {
+        self.loadingView?.display(isLoading: true)
+        feedLoader.load() { [weak self] result in
+            if let feed = try? result.get() {
+                self?.feedView?.display(feed: feed)
+            }
+            self?.loadingView?.display(isLoading: false)
+        }
+    }
+}
+
